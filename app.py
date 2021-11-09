319e0158f7c7efaa51653f9d0b9a7b68f5dbbb40
Dake Hong <dake852@gmail.com>
2021-11-09 21:31:53 +0800

    Create website views and basic operate for flask app

--- app.py
+++ app.py
@@ -1,47 +1,65 @@
 # 數字華容道 Klotski
+from flask import Flask, render_template, redirect
 from datetime import datetime
 from DigitalKlotski import DigitalKlotski
 
-moveTimes = 0 #使用者移動次數
+app = Flask(__name__)
 
-print("================================")
-print("==  Welcome to play Klotski!  ==")
-print("================================")
-print("  Press 'q' to leave this game. ")
-print("================================")
+GAME_MOVETIMES = 0 #使用者移動次數
+GAME_OBJECT = "NULL" #遊戲物件
+GAME_STATE = 0 #遊戲狀態(0=初始/1=開始/2=暫停)
 
-inputSize = input("Which Klotski's level would you want to play?(1~5):")
-klotski = DigitalKlotski(level=int(inputSize)) # 建立數字華容道
-print("OK! Klotski's level will set '%s'." %(klotski.Config["MatrixLevel"]))
-print("================================")
+@app.route('/')
+def index():
+	global GAME_OBJECT
+	GAME_OBJECT = "NULL"
+	return render_template('index.html')
 
-# 前置作業
-print("== GAME START ==")
-klotski.showQuizMatrix() # 顯示題目
+@app.route('/klotski/<int:lv>')
+def klotski(lv):
+	global GAME_OBJECT, GAME_STATE, GAME_MOVETIMES
+	if isinstance(GAME_OBJECT, str):
+		GAME_MOVETIMES = 0
+		GAME_STATE = 0
+		GAME_OBJECT = DigitalKlotski(level=int(lv)) # 建立數字華容道
 
-# 開始遊戲
-startTime = datetime.now()
-while not klotski.MatrixFinish():
-	key = input("Press block number:")
-	try:
-		success = klotski.moveBlock(int(key))
-	except:
-		success = klotski.moveBlock(0)
+	config = {
+		"lvid": lv,
+		"Level": GAME_OBJECT.Config['MatrixLevel'],
+		"Name": "{0} Klotski ({1}x{1})".format(
+			(GAME_OBJECT.Config['MatrixSize']**2) - 1, 
+			GAME_OBJECT.Config['MatrixSize']
+		),
+		"Size": GAME_OBJECT.Config['MatrixSize'],
+		"Quiz": GAME_OBJECT.getQuizMatrix()
+	}
+	games = {
+		"state": GAME_STATE,
+		"movetimes": GAME_MOVETIMES
+	}
+	return render_template('klotski.html', config=config, games=games)
+
+@app.route('/blocks/<int:key>/<int:lv>')
+def blocks(key,lv):
+	global GAME_OBJECT, GAME_MOVETIMES
+	success = GAME_OBJECT.moveBlock(int(key))
 	if success:
-		moveTimes += 1
-	print("Moves:", moveTimes)
-	klotski.showQuizMatrix() # 顯示題目
+		GAME_MOVETIMES += 1
+	return redirect('/klotski/%d'%(int(lv)))
 
-	if str(key) == "q" or str(key) == "Q":
-		break
+@app.route('/gamectl/<string:action>/<int:lv>')
+def gamectl(action,lv):
+	print(action,lv)
+	global GAME_STATE, GAME_MOVETIMES, GAME_OBJECT
+	if str(action) == "start":
+		GAME_STATE = 1
+	elif str(action) == "pause":
+		GAME_STATE = 2
+	elif str(action) == "stop":
+		GAME_MOVETIMES = 0
+		GAME_STATE = 0
+		GAME_OBJECT = DigitalKlotski(level=int(lv)) # 建立數字華容道
+	return redirect('/klotski/%d'%(int(lv)))
 
-# 遊戲結算
-endTime = datetime.now()
-time_s = (endTime - startTime).seconds
-time_ms = (endTime - startTime).microseconds
-fullsec = time_s + float(str("0.%d" %(time_ms)))
-if klotski.MatrixFinish():
-	print("Congratulation! you move %d times to finish this game." %(moveTimes))
-else:
-	print("You are not finish yet!")
-print('Spend Time (sec):', "%.3fms" %(fullsec))
+if __name__ == "__main__":
+	app.run(debug=True)
